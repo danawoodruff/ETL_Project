7e62b029a9f40ccd3b40a98e4e4a63d1d78cd965
Dana <danasnews1@gmail.com>
2021-02-08 12:35:22 -0600

    New images added and README updated

--- ERDv6.sql
+++ ERDv6.sql
@@ -1,56 +1,48 @@
-﻿
-DROP TABLE IF EXISTS "MLS";
+DROP TABLE IF EXISTS mls;
+DROP TABLE IF EXISTS school_rating;
 
-CREATE TABLE "MLS" (
-    "MLS" int   NOT NULL,
-    "Home_Type" varchar(25)   NOT NULL,
-    "Street_Number" varchar(6)   NOT NULL,
-    "Street_Name" varchar(50)   NOT NULL,
-    "City" varchar(25)   NOT NULL,
-    "Zip" int   NOT NULL,
-    "County" varchar(20),
-    "List_Price" int   NOT NULL,
-    "Market_Area" varchar(25),
-    "Area" int   NOT NULL,
-    "Subdivision" varchar(25),
-    "School_District" int   NOT NULL,
-    "Elementary" varchar(25)   NOT NULL,
-    "High_School" varchar(25)   NOT NULL,
-    "Year_Built" int,
-    "Bedrooms" int,
-    "Full_Baths" int,
-    "Half_Baths" int,
-    "Total_Baths" float,
-    "Room_Count" int,
-    "Fireplaces" int,
-    "Stories" int,
-    "Pool_Private" varchar(5),
-    "Garages" int   NOT NULL,
-    "Style" varchar(25)   NOT NULL,
-    "DOM" int   NOT NULL,
-    "CDOM" int   NOT NULL,
-    "List_Date" varchar(25)   NOT NULL,
-    CONSTRAINT "pk_MLS" PRIMARY KEY (
-        "MLS"
-     )
+CREATE TABLE school_rating (
+    Campus_Number INT   NOT NULL,
+    School  VARCHAR(50) PRIMARY KEY NOT NULL,
+    District VARCHAR(50)   NOT NULL,
+    Yrs_Unacceptable INT,
+	Rating_2019 VARCHAR(20)
 );
 
-DROP TABLE IF EXISTS "School_Rating";
+SELECT * FROM school_rating;
 
-CREATE TABLE "School_Rating" (
-    "Campus_Number" int   NOT NULL,
-    "School" varchar(50)   NOT NULL,
-    "District" varchar(50)   NOT NULL,
-    "2019_Rating" varchar(2),
-    "Yrs_Unacceptable" int,
-    CONSTRAINT "pk_School_Rating" PRIMARY KEY (
-        "School"
-     )
+CREATE TABLE mls (
+    MLS INT PRIMARY KEY NOT NULL,
+    Street_Number VARCHAR(6)   NOT NULL,
+    Street_Name VARCHAR(50)   NOT NULL,
+    City VARCHAR(25)   NOT NULL,
+    Zip INT  NOT NULL,
+    County VARCHAR(20),
+    Subdivision VARCHAR(50),
+    Home_Type VARCHAR(50)   NOT NULL,
+    Year_Built INT,
+    Bedrooms INT,
+    Full_Baths INT,
+    Half_Baths INT,
+    Total_Baths FLOAT,
+    Room_Count INT,
+    Fireplaces INT,
+    Stories INT,
+    Pool_Private VARCHAR(5),
+    Garages INT NOT NULL,
+    Style VARCHAR(250) NOT NULL,
+    List_Price INT   NOT NULL,
+    Market_Area VARCHAR(50),
+    Area VARCHAR(50)   NOT NULL,
+    DOM INT  NOT NULL,
+    CDOM INT  NOT NULL,
+    List_Date VARCHAR(25)   NOT NULL,
+    School_District VARCHAR (50) NOT NULL,
+    Elementary VARCHAR(50)   NOT NULL,
+    High_School VARCHAR(50)   NOT NULL,
+ 	FOREIGN KEY (Elementary ) REFERENCES school_rating(School),
+ 	FOREIGN KEY (High_School) REFERENCES school_rating(School)
 );
 
-ALTER TABLE "MLS" ADD CONSTRAINT "fk_MLS_Elementary" FOREIGN KEY("Elementary")
-REFERENCES "School_Rating" ("School");
-
-ALTER TABLE "MLS" ADD CONSTRAINT "fk_MLS_High_School" FOREIGN KEY("High_School")
-REFERENCES "School_Rating" ("School");
+SELECT * FROM mls;
 
